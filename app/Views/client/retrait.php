5aeaae2730af63566d6a9f94edd59f452957e84f
Elias Frederic <fredericrabemananjara47@gmail.com>
2026-07-20 13:05:01 +0300

    v1

--- app/Views/client/retrait.php
+++ app/Views/client/retrait.php
@@ -7,6 +7,18 @@
     <link rel="stylesheet" href="/assets/bootstrap/bootstrap.min.css">
 </head>
 <body class="bg-light">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-4">
+        <div class="container">
+            <a class="navbar-brand" href="/">Client</a>
+            <div class="navbar-nav">
+                <a class="nav-link" href="<?= route_to('client.dashboard') ?>">Dépôt</a>
+                <a class="nav-link" href="<?= route_to('client.retrait') ?>">Retrait</a>
+                <a class="nav-link" href="<?= route_to('client.transfert') ?>">Transfert</a>
+                <a class="nav-link" href="<?= route_to('client.historique') ?>">Historique</a>
+            </div>
+        </div>
+    </nav>
+
     <div class="container py-5">
         <div class="row justify-content-center">
             <div class="col-md-6">
