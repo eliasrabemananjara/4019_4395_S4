9ee01faebb81b173f6a381dbe18973fdf98bf91e
Elias Frederic <fredericrabemananjara47@gmail.com>
2026-07-20 10:07:53 +0300

    main maivana

---
+++ app/Views/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <a href="">Se connecter en tant que client</a><br/>
+    <a href="">Voir en tant qu'operateur</a>
+</body>
+</html>
